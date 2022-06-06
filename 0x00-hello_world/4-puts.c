4f86a12ebf31d9762e22a977845706a7eb0570c6
Molyatuu <molyatuu@gmail.com>
2022-06-06 10:53:53 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-int main(){
-/*
- * main - prints the size of various types
- * Return: 0 if exited properly, non-zero otherwise
- */
-puts("\n""Programming is like building a multilingual puzzle");
-return 0;
-}
+#include<stdio.h>
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle,\n");
+return (0);
+}  
+
 
 
