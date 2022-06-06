77f3817b2c14a02683a73b6f035dfb0ed2bfc8dc
Molyatuu <molyatuu@gmail.com>
2022-06-06 08:48:40 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+/*
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+puts("\n""Programming is like building a multilingual puzzle");
+return 0;
+}
+
+
